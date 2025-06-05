ec4d8d7a515b613222996de19edecc3f44ebdf2c
Panda2609 <banda2609@gmail.com>
2025-06-05 15:55:54 -0400

    Fix de estilo en la barra de busqueda

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ProductList from './components/ProductList';
+// import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <ProductList />
+      {/* <Dashboard />  */}
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
-import '../styles/SearchBar.css'
+import '../styles/SearchBar.css';
+import { FaSearch } from 'react-icons/fa';
 
 const SearchBar = ({ value, onChange }) => {
   return (
-    <input
-      className="search-bar-input"
-      type="text"
-      placeholder="Buscar por nombre..."
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div className="search-bar-wrapper">
+      <FaSearch className="search-bar-icon" />
+      <input
+        className="search-bar-input"
+        type="text"
+        placeholder="Buscar por nombre..."
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
   );
 };
 
